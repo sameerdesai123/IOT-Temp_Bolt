f144d2f6dbfe99d90ec6632eca33bd14d10cbec1
Sameer Desai <48178078+sameerdesai123@users.noreply.github.com>
2019-09-22 16:03:23 +0530

    removed sensitive data

--- Telegram_Module/conf.py
+++ Telegram_Module/conf.py
@@ -1,6 +1,6 @@
 """Configurations for telegram_alert.py"""
-bolt_api_key = "a98abaef-7d5a-486b-8594-ebdb099b160c"               # This is your Bolt Cloud API Key
-device_id = "BOLT7421572"                  # This is the device ID and will be similar to BOLTX where X is some numbers
-telegram_chat_id = "@temp_sam"          # This is the channel ID of the created Telegram channel. Paste after @
-telegram_bot_id = "bot870550901:AAFE8ydl0hYVx4tHJ58VLm5Vrg0IS2g9hP4"         # This is the bot ID of the created Telegram Bot. Paste after bot
-threshold = 250                     # Threshold beyond which the alert should be sent
+bolt_api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"               # This is your Bolt Cloud API Key
+device_id = "XXXXXXXXXXXXXXXXXXXXXXXXXX"                  # This is the device ID and will be similar to BOLTX where X is some numbers
+telegram_chat_id = "@XXXXXXXXXXXX"          # This is the channel ID of the created Telegram channel. Paste after @
+telegram_bot_id = "botXXXXXXXXXXXXXXXXXXXXXXXXXXXX"         # This is the bot ID of the created Telegram Bot. Paste after bot
+threshold = 300                     # Threshold beyond which the alert should be sent
